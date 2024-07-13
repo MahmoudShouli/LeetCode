970a249b4578d1b61fef47d90b6e20737e5c1126
Mahmoud Shouli <145279817+MahmoudShouli@users.noreply.github.com>
2024-07-13 21:04:02 +0300

    Time: 13 ms (26.00%), Space: 10.1 MB (5.89%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        if(s.size() != t.size()) return false;
+        
+        unordered_map<char, int> countMap;
+        
+        for(auto ch : s) {
+            countMap[ch]++;
+        }
+        
+        for(auto ch : t) {
+            if(countMap.find(ch) == countMap.end() || countMap[ch] == 0)
+                return false;
+            else
+                countMap[ch]--;
+        }
+        
+        
+        return true;
+    }
+    
+};
